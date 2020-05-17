47b4ac054576ca8b4b4084933959deffb7161568
abhishek13yadv <36642139+abhishek13yadv@users.noreply.github.com>
2020-05-17 14:04:42 +0530

    Create Palindrome Integer.cpp

---
+++ math/Palindrome Integer.cpp
@@ -0,0 +1,13 @@
+int Solution::isPalindrome(int A) {
+    if(A<0)
+        return false;
+    int x = A;
+    long long res = 0;
+    while(x){
+        res = res*10 + x%10;
+        x = x/10;
+    }
+    if(res == A)
+        return 1;
+    return 0;
+}
