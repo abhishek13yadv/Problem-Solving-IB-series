5fa2ad202e44e55915035f34a6c2abd820290e96
abhishek13yadv <36642139+abhishek13yadv@users.noreply.github.com>
2020-06-21 21:57:22 +0530

    Create length of last word.cpp

---
+++ Strings/length of last word.cpp
@@ -0,0 +1,12 @@
+int Solution::lengthOfLastWord(const string A) {
+    int count = 0;
+    int i = A.size()-1;
+    while(A[i] == ' ')
+        i--;
+    while(A[i]!=' ' && i>=0)
+    {
+        count++;
+        i--;
+    }
+    return count;
+}
